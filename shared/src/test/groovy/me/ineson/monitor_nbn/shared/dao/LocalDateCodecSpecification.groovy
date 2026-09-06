c5d4b37fd81ad0edbc1942d2811df9a9f2ab5ca0
GameyCamper <spam.spam.spam@ineson.me>
2026-09-06 15:01:15 +1000

    Improve test case formatting

--- shared/src/test/groovy/me/ineson/monitor_nbn/shared/dao/LocalDateCodecSpecification.groovy
+++ shared/src/test/groovy/me/ineson/monitor_nbn/shared/dao/LocalDateCodecSpecification.groovy
@@ -13,76 +13,76 @@ class LocalDateCodecSpecification extends Specification {
 	
 	def "getEncoderClass"() {
 		expect:
-		new LocalDateCodec().encoderClass == LocalDate.class
+            new LocalDateCodec().encoderClass == LocalDate.class
 	}
 
 	def "encode LocalDate to ISO string null check"() {
 		when:
-		def codec = new LocalDateCodec()
-		BsonWriter writer = Mock()
-		codec.encode(writer, null, null)
+            def codec = new LocalDateCodec()
+            BsonWriter writer = Mock()
+            codec.encode(writer, null, null)
 		
 		then:
-		thrown(NullPointerException)
+            thrown(NullPointerException)
 	 }
 
 	@Unroll
 	def "encode LocalDate to ISO string"(LocalDate input, String expectedResult) {
 		
 		given:
-		def codec = new LocalDateCodec()
-		BsonWriter writer = Mock()
+            def codec = new LocalDateCodec()
+            BsonWriter writer = Mock()
 	 
 		when:
-		def result = codec.encode(writer, input, null)
+            def result = codec.encode(writer, input, null)
 		 
 		then:
-        1 * writer.writeString(expectedResult)
+            1 * writer.writeString(expectedResult)
  
 		where:
-		input                      | expectedResult
-		 LocalDate.of(2023, 10, 5) | "2023-10-05"
-		 LocalDate.MIN             | "-999999999-01-01"
-		 LocalDate.MAX             | "+999999999-12-31"
+            input                      | expectedResult
+            LocalDate.of(2023, 10, 5) | "2023-10-05"
+            LocalDate.MIN             | "-999999999-01-01"
+            LocalDate.MAX             | "+999999999-12-31"
 	 }
 
 	@Unroll
 	def "decode ISO string to LocalDate tests"(String input, LocalDate expectedResult, Class<?> expectedException) {
         given:
-        def codec = new LocalDateCodec()
-		def reader = Stub(BsonReader) {
-			readString() >> input
-		} 
+            def codec = new LocalDateCodec()
+            def reader = Stub(BsonReader) {
+                readString() >> input
+            }
 
         when:
-        def result = null
-        def resultException = null
-		try {
-		     result = codec.decode(reader, null)
-		} catch (e) {
-			resultException = e
-		}
+            def result = null
+            def resultException = null
+            try {
+                result = codec.decode(reader, null)
+            } catch (e) {
+                resultException = e
+            }
 		
 		then:
-		if (expectedResult) {
-			assert resultException == null
-			assert expectedResult == result
-		} else {
-			assert expectedException == resultException.class
-		}
+            if (expectedResult) {
+                assert resultException == null
+                assert expectedResult == result
+            } else {
+                assert expectedException == resultException.class
+            }
 
 		where:
-		input              | expectedResult              | expectedException
-		"2023-10-05"       | LocalDate.of(2023, 10, 5)   | null
-		"-0023-10-05"      | LocalDate.of(-23, 10, 5)    | null
-		"-999999999-01-01" | LocalDate.MIN               | null
-		"+999999999-12-31" | LocalDate.MAX               | null
-		"2025-2-9"         | null                        | DateTimeParseException.class
-		"-2025-2-9"        | null                        | DateTimeParseException.class
-		"2023-13-32"       | null                        | DateTimeParseException.class
-		"202332-01-02"     | null                        | DateTimeParseException.class
-		"rubbish"          | null                        | DateTimeParseException.class
-		null               | null                        | NullPointerException.class
+            input              | expectedResult              | expectedException
+            "2023-10-05"       | LocalDate.of(2023, 10, 5)   | null
+            "-0023-10-05"      | LocalDate.of(-23, 10, 5)    | null
+            "-999999999-01-01" | LocalDate.MIN               | null
+            "+999999999-12-31" | LocalDate.MAX               | null
+            "2025-2-9"         | null                        | DateTimeParseException.class
+            "-2025-2-9"        | null                        | DateTimeParseException.class
+            "2023-13-32"       | null                        | DateTimeParseException.class
+            "202332-01-02"     | null                        | DateTimeParseException.class
+            "rubbish"          | null                        | DateTimeParseException.class
+            null               | null                        | NullPointerException.class
     }
 
 }
--- shared/src/test/groovy/me/ineson/monitor_nbn/shared/dao/LocalDateTimeCodecSpecification.groovy
+++ shared/src/test/groovy/me/ineson/monitor_nbn/shared/dao/LocalDateTimeCodecSpecification.groovy
@@ -13,47 +13,47 @@ class LocalDateTimeCodecSpecification extends Specification {
 	
 	def "getEncoderClass"() {
 		expect:
-		new LocalDateTimeCodec().encoderClass == LocalDateTime.class
+            new LocalDateTimeCodec().encoderClass == LocalDateTime.class
 	}
 
 	def "encode LocalDateTime to ISO string null check"() {
 		when:
-		def codec = new LocalDateTimeCodec()
-		BsonWriter writer = Mock()
-		codec.encode(writer, null, null)
+            def codec = new LocalDateTimeCodec()
+            BsonWriter writer = Mock()
+            codec.encode(writer, null, null)
 		
 		then:
-		thrown(NullPointerException)
+            thrown(NullPointerException)
 	 }
 
 	@Unroll
 	def "encode LocalDateTime to ISO string"(LocalDateTime input, String expectedResult) {
 		
 		given:
-		def codec = new LocalDateTimeCodec()
-		BsonWriter writer = Mock()
+            def codec = new LocalDateTimeCodec()
+            BsonWriter writer = Mock()
 	 
 		when:
-		def result = codec.encode(writer, input, null)
+            def result = codec.encode(writer, input, null)
 		 
 		then:
-        1 * writer.writeString(expectedResult)
+            1 * writer.writeString(expectedResult)
  
 		where:
-		input                                      | expectedResult
-		 LocalDateTime.of(2023, 10, 5, 1, 2)       | "2023-10-05T01:02"
-		 LocalDateTime.of(2022, 9, 25, 19, 2, 44)  | "2022-09-25T19:02:44"
-		 LocalDateTime.MIN                         | "-999999999-01-01T00:00"
-		 LocalDateTime.MAX                         | "+999999999-12-31T23:59:59.999999999"
+            input                                      | expectedResult
+            LocalDateTime.of(2023, 10, 5, 1, 2)       | "2023-10-05T01:02"
+            LocalDateTime.of(2022, 9, 25, 19, 2, 44)  | "2022-09-25T19:02:44"
+            LocalDateTime.MIN                         | "-999999999-01-01T00:00"
+            LocalDateTime.MAX                         | "+999999999-12-31T23:59:59.999999999"
 	 }
 
 	@Unroll
 	def "decode ISO string to LocalDateTime tests"(String input, LocalDateTime expectedResult, Class<?> expectedException) {
         given:
-        def codec = new LocalDateTimeCodec()
-		def reader = Stub(BsonReader) {
-			readString() >> input
-		} 
+            def codec = new LocalDateTimeCodec()
+            def reader = Stub(BsonReader) {
+                readString() >> input
+            }
 
         when:
         def result = null
@@ -65,26 +65,26 @@ class LocalDateTimeCodecSpecification extends Specification {
 		}
 		
 		then:
-		if (expectedResult) {
-			assert resultException == null
-			assert expectedResult == result
-		} else {
-			assert expectedException == resultException.class
-		}
+            if( expectedResult) {
+                assert resultException == null
+                assert expectedResult == result
+            } else {
+                assert expectedException == resultException.class
+            }
 
 		where:
-		input                                 | expectedResult                            | expectedException
-		"2023-10-05T10:03:13"                 | LocalDateTime.of(2023, 10, 5, 10 , 3, 13) | null
-		"-0023-10-05T21:57"                   | LocalDateTime.of(-23, 10, 5, 21, 57)      | null
-		"-999999999-01-01T00:00:00.0"         | LocalDateTime.MIN                         | null
-		"+999999999-12-31T23:59:59.999999999" | LocalDateTime.MAX                         | null
-		"2025-2-9T2:2"                        | null                                      | DateTimeParseException.class
-		"-2025-2-9"                           | null                                      | DateTimeParseException.class
-		"2023-13-32T00:00"                    | null                                      | DateTimeParseException.class
-		"2023-11-11T0:0"                      | null                                      | DateTimeParseException.class
-		"2023-11-11T01"                       | null                                      | DateTimeParseException.class
-		"rubbish"                             | null                                      | DateTimeParseException.class
-		null                                  | null                                      | NullPointerException.class
+            input                                 | expectedResult                            | expectedException
+            "2023-10-05T10:03:13"                 | LocalDateTime.of(2023, 10, 5, 10 , 3, 13) | null
+            "-0023-10-05T21:57"                   | LocalDateTime.of(-23, 10, 5, 21, 57)      | null
+            "-999999999-01-01T00:00:00.0"         | LocalDateTime.MIN                         | null
+            "+999999999-12-31T23:59:59.999999999" | LocalDateTime.MAX                         | null
+            "2025-2-9T2:2"                        | null                                      | DateTimeParseException.class
+            "-2025-2-9"                           | null                                      | DateTimeParseException.class
+            "2023-13-32T00:00"                    | null                                      | DateTimeParseException.class
+            "2023-11-11T0:0"                      | null                                      | DateTimeParseException.class
+            "2023-11-11T01"                       | null                                      | DateTimeParseException.class
+            "rubbish"                             | null                                      | DateTimeParseException.class
+            null                                  | null                                      | NullPointerException.class
     }
 
 }
